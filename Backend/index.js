1112350f3c55ac138d14e7da428630389abebc07
Harika <harikapuli95@gmail.com>
2023-09-02 23:30:26 +0530

    added cache data base

--- Backend/index.js
+++ Backend/index.js
@@ -2,23 +2,57 @@ import  express  from 'express';
 import fetch from 'node-fetch';
 var router = express.Router();
 var app = express();
-   app.get("/weather",async (req, res)    => {
-    const myArray = req.query.location.split(',');
-    const newtimestep = req.query.timesteps;
-    const r = await GetWeather(myArray[0],myArray[1], newtimestep);
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+
+app.use(express.json());
+dotenv.config();
+
+const client = new MongoClient(process.env.DATABASE_URL);
+
+client.connect();
+var db = client.db("weather");
+
+ app.get("/weather",async (req, res)    => {
+     
+   await ReadData(req.query.location);
+    const r = await GetWeather(req.query.location);
     res.send(r);
    
 })
 
-async function GetWeather(lat,long,timesteps)
+ async function ReadData(location)
 {
-    
-    let url = `https://api.tomorrow.io/v4/timelines?location=${lat},${long}&fields=temperature&timesteps=1h&units=metric&apikey=okB7Y1GRD5kZMXyz4BPeCWRl6XI7dmes`
-     const response = await fetch(url);
-    const text = await response.text();
-    return text;
+    var data =db.collection("location").find({},function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
 
+            console.log(JSON.stringify(result));
+        }
+    })
+    return data;
 }
-app.listen(9011, function () {
-    console.log("server started!");
+
+async function GetWeather(location)
+{
+    //if found in db return db value else getweather method.
+    let url = `https://api.tomorrow.io/v4/timelines?location=${location}&fields=temperature&timesteps=1h&units=metric&apikey=okB7Y1GRD5kZMXyz4BPeCWRl6XI7dmes`
+    const response = await fetch(url);
+    const responseData = await response.text();
+    var _key = location;
+    var myobj = {_key , responseData};
+    db.collection("location").insertOne(myobj, function(err, res) {
+        if (err) 
+        {
+            console.log("error = "+err);
+            throw err;
+        }
+        console.log("1 document inserted");
+    });
+    return responseData;
+}
+app.listen(process.env.PORT_NO, () => {
+    console.log("server started on " + process.env.PORT_NO);
 })
